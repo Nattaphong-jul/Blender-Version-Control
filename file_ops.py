a951d0006cbae1a5a5f4fd0d89a5d4abeedda3e4
Nattaphong-jul <nattapong.jullaya@gmail.com>
2026-04-28 18:15:49 +0700

    Add Roll Back core Function

--- file_ops.py
+++ file_ops.py
@@ -3,7 +3,7 @@ import shutil
 import datetime
 import json
 
-def copy_file(source_path, destination_folder):
+def copy_file(source_path, destination_folder, description=""):
     timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S") # Format: YYYYMMDD_HHMMSS
     file_name = os.path.basename(source_path)
     manifest_path = os.path.join(destination_folder, "manifest.json")
@@ -14,7 +14,7 @@ def copy_file(source_path, destination_folder):
     entries.append({
                 "version_id": timestamp,
                 "timestamp": datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S"), # Format: DD-MM-YYYY HH:MM:SS
-                "description": "",
+                "description": description,
                 "file_name": file_name
             })
     write_manifest(manifest_path, entries)
@@ -31,6 +31,27 @@ def write_manifest(manifest_path, entries):
     with open(manifest_path, "w") as manifest_file:
         json.dump(entries, manifest_file, indent=2)
 
+def roll_back(target_file, destination_folder, selected_id):
+    manifest_path = os.path.join(destination_folder, "manifest.json")
+    entries = load_manifest(manifest_path)
+
+    for entry in entries:
+        if entry["version_id"] == selected_id:
+            version_folder = os.path.join(destination_folder, entry["version_id"])
+            source_file = os.path.join(version_folder, entry["file_name"])
+            
+            if os.path.exists(source_file):
+                # Copy latest version before rolling back
+                copy_file(target_file, destination_folder, description=f"Roll Back")
+
+                # Roll Back
+                shutil.copy(source_file, target_file)
+                print(f"Successfully rolled back to version: {selected_id}")
+            else:
+                print(f"Error: Version file not found at {source_file}")
+            break
+
 source = os.path.join(os.path.dirname(__file__), "example_file.txt")
 destination = os.path.join(os.path.dirname(__file__), "test")
-copy_file(source, destination)
+# copy_file(source, destination)
+roll_back(source, destination, "20260428_173735")
