f25c3e7f51696f65c61378f39ebdde57bdee546b
Nattaphong-jul <nattapong.jullaya@gmail.com>
2026-04-28 16:22:52 +0700

    Add timestamp as folder name, copy file inside

--- file_ops.py
+++ file_ops.py
@@ -1,7 +1,9 @@
 import os
 import shutil
+import datetime
 
 def copy_file(source_path, destination_folder):
+    destination_folder = os.path.join(destination_folder, datetime.datetime.now().strftime("%Y%m%d_%H%M%S"))
     os.makedirs(destination_folder, exist_ok=True)
     shutil.copy(source_path, destination_folder)
     print(f"Copied {os.path.basename(source_path)} to {destination_folder}")
