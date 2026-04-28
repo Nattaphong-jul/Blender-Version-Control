f3091edbcfe3564ac9598a8774abee5da903cedc
Nattaphong-jul <nattapong.jullaya@gmail.com>
2026-04-28 17:18:23 +0700

    Add File Copy core function  and Manifest read/write

--- file_ops.py
+++ file_ops.py
@@ -1,12 +1,35 @@
 import os
 import shutil
 import datetime
+import json
 
 def copy_file(source_path, destination_folder):
-    destination_folder = os.path.join(destination_folder, datetime.datetime.now().strftime("%Y%m%d_%H%M%S"))
-    os.makedirs(destination_folder, exist_ok=True)
-    shutil.copy(source_path, destination_folder)
-    print(f"Copied {os.path.basename(source_path)} to {destination_folder}")
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S") # Format: YYYYMMDD_HHMMSS
+    file_name = os.path.basename(source_path)
+    manifest_path = os.path.join(destination_folder, "manifest.json")
+
+    os.makedirs(os.path.join(destination_folder, timestamp), exist_ok=True)
+
+    entries = load_manifest(manifest_path)
+    entries.append({
+                "version_id": timestamp,
+                "timestamp": datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S"), # Format: DD-MM-YYYY HH:MM:SS
+                "description": "",
+                "file_name": file_name
+            })
+    write_manifest(manifest_path, entries)
+
+    shutil.copy(source_path, os.path.join(destination_folder, timestamp))
+
+def load_manifest(manifest_path) -> list:
+    if os.path.exists(manifest_path):
+        with open(manifest_path, "r") as manifest_file:
+            return json.load(manifest_file)
+    return []
+
+def write_manifest(manifest_path, entries):
+    with open(manifest_path, "w") as manifest_file:
+        json.dump(entries, manifest_file, indent=2)
 
 source = os.path.join(os.path.dirname(__file__), "example_file.txt")
 destination = os.path.join(os.path.dirname(__file__), "test")
