1a5febd89a22ad317ea7518e3071b1d50cb496d4
Nattaphong-jul <nattapong.jullaya@gmail.com>
2026-04-29 10:50:17 +0700

    Add Copy/Roll Back function to main file

--- main.py
+++ main.py
@@ -5,13 +5,74 @@ bl_info = {
 }
 
 import bpy
+import os
+import shutil
+import datetime
+import json
+
+def copy_file(source_path, destination_folder, description=""):
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S") # Format: YYYYMMDD_HHMMSS
+    file_name = os.path.basename(source_path)
+    manifest_path = os.path.join(destination_folder, "manifest.json")
+
+    os.makedirs(os.path.join(destination_folder, timestamp), exist_ok=True)
+
+    entries = load_manifest(manifest_path)
+    entries.append({
+                "version_id": timestamp,
+                "timestamp": datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S"), # Format: DD-MM-YYYY HH:MM:SS
+                "description": description,
+                "file_name": file_name
+            })
+    write_manifest(manifest_path, entries)
+
+    shutil.copy(source_path, os.path.join(destination_folder, timestamp))
+
+def load_manifest(manifest_path) -> list:
+    if os.path.exists(manifest_path):
+        with open(manifest_path, "r") as manifest_file:
+            return json.load(manifest_file)
+    return []
+
+def write_manifest(manifest_path, entries):
+    with open(manifest_path, "w") as manifest_file:
+        json.dump(entries, manifest_file, indent=2)
+
+def roll_back(target_file, destination_folder, selected_id):
+    manifest_path = os.path.join(destination_folder, "manifest.json")
+    entries = load_manifest(manifest_path)
+
+    for entry in entries:
+        if entry["version_id"] == selected_id:
+            version_folder = os.path.join(destination_folder, entry["version_id"])
+            source_file = os.path.join(version_folder, entry["file_name"])
+            
+            if os.path.exists(source_file):
+                # Copy latest version before rolling back
+                copy_file(target_file, destination_folder, description=f"Roll Back")
+
+                # Roll Back
+                shutil.copy(source_file, target_file)
+                print(f"Successfully rolled back to version: {selected_id}")
+            else:
+                print(f"Error: Version file not found at {source_file}")
+            break
+
 
 class BVC_OT_SaveVersion(bpy.types.Operator):
     bl_idname = "bvc.save_version"
     bl_label  = "Save Version"
 
     def execute(self, context):
-        print("Save Version clicked!")
+        blend_path = bpy.data.filepath
+        if not blend_path:
+            self.report({"ERROR"}, "Please save your file first!")
+            return {"CANCELLED"}
+    
+        destination_folder = os.path.join(os.path.dirname(blend_path), ".bvc", "versions")
+        copy_file(blend_path, destination_folder)
+
+        self.report({"INFO"}, "Version saved!")
         return {"FINISHED"}
 
 class BVC_PT_Panel(bpy.types.Panel):
